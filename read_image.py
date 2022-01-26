97003e4fd1765e61f477717be21d88cb3dada61f
Tomas Martincic <66355160+Martincic@users.noreply.github.com>
2022-01-26 19:10:03 +0100

    WebCam live capture

--- read_image.py
+++ read_image.py
@@ -7,60 +7,111 @@ import os
 MIN_MATCHES = 15
 
 WINDOW_NAME = "test window"
-
 workingDir = os. getcwd()+'/'
+
+
+def rescaleFrame(frame, scale=0.75):
+    # frame.shape[1] is width of the image
+    width = int(frame.shape[1] * scale)
+
+    # frame.shape[0] is height of the image
+    height = int(frame.shape[0] * scale)
+
+    return cv.resize(frame, (width,height), interpolation=cv.INTER_AREA)
+
+
+# Capture photo to be used as model
+cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Our operations on the frame come here
+    # gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    if cv.waitKey(1) & 0xFF == ord('y'): #save on pressing 'y' 
+        cv.imwrite('static/img/models/capture.png',frame)
+        break
+
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
 
 # To read image from disk, we use
 # cv2.imread function, in below method,
 
-for model in os.listdir(workingDir + 'static/img/models'):
-    for filename in os.listdir(workingDir + 'static/img/samples'):
-        sample_img = cv.imread(workingDir + 'static/img/models/'+model) #param2 0 for black and white
-        output_img = cv.imread(workingDir + 'static/img/samples/'+filename)
-        if sample_img is None: sys.exit("..no sample image found")
-        if output_img is None: sys.exit("..no output image found")
+cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    
+    k = cv.waitKey(1)
+    # CLOSE ON 'q'
+    if k == ord("q"):
+        # delete created window from screen
+        cap.release()
+        cv.destroyAllWindows()
 
-        # border, random img decoration shit
-        # img = cv.copyMakeBorder(img, 10, 10, 10, 10, cv.BORDER_CONSTANT, None, value = 0)
+    # sample_img = cv.imread(workingDir + 'static/img/models/'+model) #param2 0 for black and white
+    sample_img = cv.imread(workingDir + 'static/img/models/capture.png')
+    # output_img = cv.imread(workingDir + 'static/img/samples/'+filename)
+    output_img = frame
+    if sample_img is None: sys.exit("..no sample image found")
+    if output_img is None: sys.exit("..no output image found")
 
-        # window, second param by default WINDOW_AUTOSIZE
-        cv.namedWindow(WINDOW_NAME)
+    # border, random img decoration shit
+    # img = cv.copyMakeBorder(img, 10, 10, 10, 10, cv.BORDER_CONSTANT, None, value = 0)
 
-        #works without time and threads, but errors in closing 
-        start = time.time()
-        cv.startWindowThread()
+    # window, second param by default WINDOW_AUTOSIZE
+    cv.namedWindow(WINDOW_NAME)
 
-        # Initiate ORB detector
-        orb = cv.ORB_create()
+    #works without time and threads, but errors in closing 
+    start = time.time()
+    cv.startWindowThread()
 
-        # create brute force  matcher object
-        bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+    # Initiate ORB detector
+    orb = cv.ORB_create()
 
-        # Compute model keypoints and its descriptors
-        kp_model, des_model = orb.detectAndCompute(sample_img, None)  
-        # Compute scene keypoints and its descriptors
-        kp_frame, des_frame = orb.detectAndCompute(output_img, None)
-        # Match frame descriptors with model descriptors
-        matches = bf.match(des_model, des_frame)
-        # Sort them in the order of their distance
-        matches = sorted(matches, key=lambda x: x.distance)
+    # create brute force  matcher object
+    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
 
-        if len(matches) > MIN_MATCHES:
-            # draw first 15 matches.
-            output_img = cv.drawMatches(sample_img, kp_model, output_img, kp_frame,
-                                matches[:MIN_MATCHES], 0, flags=2)
-            # show result
-            cv.imshow(WINDOW_NAME, output_img)
-            # if param 0 wait forever
-            k = cv.waitKey(0)
+    # Compute model keypoints and its descriptors
+    kp_model, des_model = orb.detectAndCompute(sample_img, None)  
+    # Compute scene keypoints and its descriptors
+    kp_frame, des_frame = orb.detectAndCompute(output_img, None)
+    # Match frame descriptors with model descriptors
+    matches = bf.match(des_model, des_frame)
+    # Sort them in the order of their distance
+    matches = sorted(matches, key=lambda x: x.distance)
 
-            # CLOSE ON 'q'
-            if k == ord("q"):
-                # delete created window from screen
-                cv.destroyAllWindows()
-        else:
-            print("Not enough matches have been found - %d/%d" % (len(matches),
-                                                                MIN_MATCHES))
+    if len(matches) > MIN_MATCHES:
+        # draw first 15 matches.
+        output_img = cv.drawMatches(sample_img, kp_model, output_img, kp_frame,
+                            matches[:MIN_MATCHES], 0, flags=2)
+        # resized_img = rescaleFrame(output_img, 0.5)
+        # show result 
+        cv.imshow(WINDOW_NAME, output_img)
+        # if param 0 wait forever
+    else:
+        print("Not enough matches have been found - %d/%d" % (len(matches),
+                                                            MIN_MATCHES))
 
 
     # # find the keypoints with ORB (no compute)
