d993124047af642e26746fae63175a4e2b5b522d
Martincic <martincic.tomas@gmail.com>
2022-02-07 20:31:54 +0100

    python http server

--- read_image.py
+++ read_image.py
@@ -1,36 +1,48 @@
+#!/usr/bin/env python
 from importlib.machinery import SOURCE_SUFFIXES
 import cv2 as cv
 import time
-import sys
-import os
-import socket
-import struct
+import sys 
 
-address = ("192.168.0.32", 8080)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(address)
-s.listen(1000)
-print(1)
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+
+  # GET
+  def do_GET(self):
+    # Send response status code
+    self.send_response(200)
+
+    # Send message back to client
+    message = bytes(
+      str(self.headers) +
+      "\n" +
+      self.requestline +
+      "\n"
+      , 'utf8')
+
+    # Send headers
+    self.send_header('Content-type','text/plain; charset=utf-8')
+    self.send_header('Content-length', str(len(message)))
+    self.end_headers()
+
+    # Write content as utf-8 data
+    self.wfile.write(message)
+    return
+
+def run():
+  print('starting server...')
+
+  # Server settings
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+  server_address = ('192.168.0.32', 8081)
+  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  print('running server...')
+  httpd.serve_forever()
 
 
-client, addr = s.accept()
-
-print('accept')
-# print ('got connected from %s' % addr)
-
-with open('tst.jpg', 'wb') as img:
-    print("open jpg")
-    while True:
-        print('RECEIVING')
-        data = client.recv(1024)
-        if not data:
-            break
-        img.write(data)
-print ('received, yay!')
-
-client.close()
-
-
+run()
 
 
 
