d8bfda2454fa1614d73a11e48ff913b47dcaafec
Martincic <martincic.tomas@gmail.com>
2022-02-19 23:21:49 +0100

    something| |

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from distutils.log import debug
 from flask import Flask, render_template
 from flask_ngrok import run_with_ngrok
+from threading import Thread
+from time import sleep
+from flask import request
+import numpy as np
+import cv2
 
 app = Flask(__name__)
 run_with_ngrok(app)
@@ -9,5 +14,22 @@ run_with_ngrok(app)
 def index():
     return render_template('index.html')
   
+@app.route('/maskImage' , methods=['POST'])
+def mask_image():
+	# print(request.files , file=sys.stderr)
+	file = request.files['image'].read() ## byte file
+	npimg = np.fromstring(file, np.uint8)
+	img = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
+	######### Do preprocessing here ################
+	# img[img > 150] = 0
+	## any random stuff do here
+	################################################
+	img = Image.fromarray(img.astype("uint8"))
+	rawBytes = io.BytesIO()
+	img.save(rawBytes, "JPEG")
+	rawBytes.seek(0)
+	img_base64 = base64.b64encode(rawBytes.read())
+	return jsonify({'status':str(img_base64)})
+  
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run()
--- static/script.js
+++ static/script.js
@@ -50,6 +50,7 @@
         try {
             videoStream = await navigator.mediaDevices.getUserMedia(constraints);
             video.srcObject = videoStream;
+            
         } catch (err) {
             alert("Could not access the camera");
         }
@@ -57,3 +58,48 @@
 
     initializeCamera();
 })();
+
+
+setInterval(myTimer, 5000);
+let counter = 0;
+
+function myTimer() {
+    counter++;
+    document.getElementById('counter').innerHTML = counter;
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var video = document.getElementById('video');
+    ctx.drawImage(video,0,0)
+    ctx.translate(video,0)
+
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(20, 20, 150, 100);
+
+    document.getElementById("canvas").style = "display:none;"
+    //ctx.drawImage(canvas,0,0)
+    data = canvas.toDataURL("image/JPEG");
+
+    console.log(data)
+    let req = $.ajax({
+        type: "POST",
+        url: "/maskImage",
+        data: data,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            if (data.success) {
+                alert('Your file was successfully uploaded!');
+            } else {
+               alert('There was an error uploading your file!');
+            }
+        },
+        error: function (data) {
+            console.log('There was an error uploading your file!');
+        }
+    }).done(function () {
+        console.log("Sent");
+        console.log(this);
+    });
+}    
